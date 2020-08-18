1a78fb31f20fc4fbbce05bc384863648d7b16dbe
Cliffart <Cliffart44@gmail.com>
2020-08-18 16:49:10 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket_price_kpks;
+        ticket_price_kpks = 20_000_00;
+        int bonus_miles = ticket_price_kpks / 20 / 100;
+        System.out.println(bonus_miles);
+    }
+}
